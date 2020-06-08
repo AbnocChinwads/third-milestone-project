1f0bb066a209fb6a5457945ca437628f6862858a
AbnocChinwads <rnemo@hotmail.co.uk>
2020-06-08 14:23:00 +0000

    Removed env.py file from tracking

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["MONGO_URI"] = "mongodb+srv://root:wF2QWAxBUuKcorui@myfirstcluster-lniy2.mongodb.net/third_milestone_project?retryWrites=true&w=majority"
-
-os.environ["SECRET_KEY"] = "b'N(O\xaf\xbf\x1a\xccMG\xb8\x83Y\xc6\xa6\x0c\xd5'"
