852f360ca66b52321ebc3392fc6515ce52e90d75
AbnocChinwads <rnemo@hotmail.co.uk>
2020-06-11 22:40:20 +0000

    Attempted to add a 'more info' page

--- app.py
+++ app.py
@@ -16,11 +16,35 @@ mongo = PyMongo(app)
 # Tasks
 
 
-@app.route("/")  # Display main task page
+@app.route("/")  # Display main page with paginated list
 @app.route("/get_creatures")
 def get_creatures():
     return render_template("index.html",
                            creatures=mongo.db.creatures.find())
+
+
+"""
+Display more in depth information on creatures such as:
+stats, vulnerabilities, skills, actions etc.
+"""
+
+
+@app.route("/<creature_id>")
+def more_info(creature_id):
+    creature = mongo.db.creatures
+    creature.find({"_id": ObjectId(creature_id)},
+                  {
+        "creature_name": request.form.get("creature_name"),
+        "challenge_rating": request.form.get("challenge_rating"),
+        "creautre_type": request.form.get("creature_type"),
+        "creature_size": request.form.get("creature_size"),
+        "creature_ac": request.form.get("creature_ac"),
+        "creautre_hp": request.form.get("creature_hp"),
+        "creature_speed": request.form.get("creature_speed"),
+        "creature_alignment": request.form.get("creature_alignment"),
+        "legendary": request.form.get("legendary")
+    })
+    return render_template("moreinfo.html")
 
 
 @app.route("/login")
