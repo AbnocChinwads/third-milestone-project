1b6fe892710569d9eae0f4ea689b5b39852b861f
AbnocChinwads <rnemo@hotmail.co.uk>
2020-06-17 19:19:39 +0000

    Added list of names, attempted further listing

--- app.py
+++ app.py
@@ -18,23 +18,33 @@ mongo = PyMongo(app)
 
 
 @app.route("/")  # Display main page with paginated list
-@app.route("/get_story")
-def get_story():
+@app.route("/game_list")
+def game_list():
     return render_template("index.html",
-                           creatures=mongo.db.creatures.find())
+                           game_list=mongo.db.game_list.find())
+
+
+@app.route("/moreinfo/<game_list_id>")
+def more_info(game_list_id):
+    mongo.db.game_list.find_one({"_id": ObjectId(game_list_id)})
+    return render_template("moreinfo.html",
+                           game_list=mongo.db.game_list.find())
+
 
 """
 Add in the CRUD architecture, and settle on what data to use for the database.
-It has to be sensible, you can't keep jumping around like this between ridiculous ideas.
-Pick some information that lends itself to being created, read, updated, and deleted...
-not some high fancy idea of reworking the Monster Manual or getting some kind of open source
+It has to be sensible, you can't keep jumping around like this between
+ridiculous ideas. Pick some information that lends itself to being
+created, read, updated, and deleted... not some high fancy idea
+of reworking the Monster Manual or getting some kind of open source
 writing project off the ground.
 
 
 @app.route("create_data")
 def create_data():
     mongo.db.create_one()
-    return redirect("index", add modal/alert with message 'your data has been added')
+    return redirect("index",
+                    add modal/alert with message 'your data has been added')
 
 Use objectId to find the data they are looking for.
 Maybe use a search element in the header for this.
@@ -45,17 +55,20 @@ def get_data():
     return redirect("index")
 
 
-@app.route("/delete_data")
-def delete_data():
-    mongo.db.database.remove_one(database_id)
-    return redirect("index", add modal/alert 'the entry has been deleted')
+@app.route("/delete_review/<review_id>")  # Delete a review from the database
+def delete_review(review_id):
+    mongo.db.reviews.remove({"_id": ObjectId(review_id)})
+    return redirect(url_for("get_reviews"),
+                    add modal/alert 'the entry has been deleted')
 
-use less aggressive function to delete a subset of data from a document
-by using the key to find the value you wish to remove... use similar method to update document.
+use less aggressive function to delete a subset of data from a
+document by using the key to find the value you wish to remove...
+use similar method to update document.
 """
+# Create login system, and use database to store usernames and passwords
 
 
-@app.route("/login")  # Create login system, and use database to store usernames and passwords
+@app.route("/login")
 def login():
     return render_template("login.html")
 
@@ -65,7 +78,7 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contactus")  # Contact information page using an email API
+@app.route("/contactus")  # Contact information page poss using an email API
 def contact():
     return render_template("contact.html")
 
