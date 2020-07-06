2eae3ab55fce65e1f1ae8786a4dd0905f047079e
AbnocChinwads <rnemo@hotmail.co.uk>
2020-07-06 19:03:31 +0000

    Fixed reviews not displaying

--- app.py
+++ app.py
@@ -42,18 +42,20 @@ def insert_game():
 def more_info(game_list_id):
     the_game = mongo.db.game_list.find_one({"_id": ObjectId(game_list_id)})
     all_categories = mongo.db.game_list.find()
-    reviews = mongo.db.reviews.aggregate([
-        {"$lookup":
-         {
-             "from": "game_list",
-             "localField": "game_name",
-             "foreignField": "game_name",
-             "as": "game_name"
-         }
-         }
-    ])
+    if "game_name" == "game_name":
+        reviews = mongo.db.reviews.aggregate([
+                {"$lookup":
+                    {
+                        "from": "game_list",
+                        "localField": "game_name",
+                        "foreignField": "game_name",
+                        "as": "game_name"
+                    }
+                 }
+            ])
+    result = list(reviews)
     return render_template("moreinfo.html", game=the_game,
-                           categories=all_categories, review=reviews)
+                           categories=all_categories, review=result)
 
 
 # Display form page to edit game
