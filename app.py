81d7da0280cab437b66ae879c5bbca30d12dce01
AbnocChinwads <rnemo@hotmail.co.uk>
2020-07-09 14:38:07 +0000

    Set debug to false

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ def contact():
 
 if __name__ == "__main__":
     app.run(host=os.getenv("IP"),
-            port=int(os.getenv("PORT")), debug=True)
+            port=int(os.getenv("PORT")), debug=False)
