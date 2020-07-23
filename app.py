7fba1fd278ec1c0fe94f8255fe3f954d22150ab6
AbnocChinwads <rnemo@hotmail.co.uk>
2020-07-23 14:24:33 +0000

    Adjusted comments

--- app.py
+++ app.py
@@ -134,14 +134,40 @@ def delete_review(review_id):
 """
 Add in the CRUD architecture:
 
-* Create: Games - Added (Works)
-          Reviews - Added (Works, have to return to the main page)
-* Read: Games - Added (Works)
-        Reviews - Added (Now displays game specific reviews)
-* Update: Games - Added (Works)
-          Reviews - Added (Works, have to return to the main page)
-* Delete: Games - Added (Works)
-          Reviews - Added (Works, have to return to the main page)
+* Create: Reviews - Added (Works, have to return to the main page)
+* Read: Done
+* Update: Reviews - Added (Works, have to return to the main page)
+* Delete: Reviews - Added (Works, have to return to the main page)
+
+TODO:
+
+https://www.codementor.io/@arpitbhayani/fast-and-efficient-pagination-in-mongodb-9095flbqr
+
+def idlimit(page_size, last_id=None):
+    page_size = 2
+    # Function returns `page_size` number of documents after last_id
+    and the new last_id. #
+
+    if last_id is None:
+        # When it is first page
+        cursor = mongo.db['game_list'].find().limit(page_size)
+    else:
+        cursor = mongo.db['game_list'].find(
+                {'_id': {'$gt': last_id}}).limit(page_size)
+
+    # Get the data
+    data = [x for x in cursor]
+
+    if not data:
+        # No documents left
+        return None, None
+
+    # Since documents are naturally ordered with _id, last document will
+    # have max id.
+    last_id = data[-1]['_id']
+
+    # Return data and last_id
+    return data, last_id
 
 TODO:
     * Create a function to delete a single key:value pair from a
